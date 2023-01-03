cbed7a4314f9f3d92449081ad9f8d202f156e941
Julian <julianibibo@gmail.com>
2023-01-03 16:27:10 +0100

    test

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-const input = document.getElementById('fileInput');
-const table = document.getElementById('table');
+const input = document.getElementById("fileInput");
+const table = document.getElementById("table");
 
 let data = [];
-let users = [];
+let users = [
+  { id: 1, name: "daan", transactions: [] },
+  { id: 2, name: "tim", transactions: [] },
+];
 let currentlySelectedTransaction = null;
 
 const csvString = `type,amount,description
 deposit,10,tim
 withdraw,20,"hello i need money kind regards julian"
 activity,10,beer
-deposit,10,daans`
+deposit,10,daans`;
 
-console.log(csvString)
-// Add users in the users id div
-const addUser = (event) => {
+// Handle adding user to the users array
+const handleAddUser = (event) => {
   event.preventDefault();
 
   // Check if empty, dont add
-  if (event.target.name.value === '')
-    return;
+  if (event.target.name.value === "") return;
 
-  // Get form data
-  const formData = new FormData(event.target);
-  const name = formData.get('name');
-  
   const user = {
     id: users.length + 1,
-    name,
-    transactions: []
-  }
+    name: event.target.name.value,
+    transactions: [],
+  };
 
   // Add user to users array
   users.push(user);
 
   // Clean input box
   event.target.reset();
-  displayUsers(user)
-}
+  refreshUsersDiv();
+};
 
 // input.addEventListener('change', function() {
 //   const file = input.files[0];
@@ -51,27 +48,29 @@ const addUser = (event) => {
 // });
 
 const setTableData = (data) => {
-  const tableData = data.map((transaction) => {
-    console.log(transaction)
-    const { type, amount, description, id } = transaction;
-    return `
+  const tableData = data
+    .map((transaction) => {
+      console.log(transaction);
+      const { type, amount, description, id } = transaction;
+      return `
       <tr onclick="handleTransaction(${id})" data-id="${id}">
         <td>${type}</td>
         <td>${amount}</td>
         <td>${description}</td>
       </tr>
     `;
-  }).join('');
+    })
+    .join("");
   table.innerHTML = tableData;
-}
+};
 
 function parseCSV(csvString) {
   // Split the CSV string on new lines to get an array of rows
-  let rows = csvString.split('\n'); 
-  console.log(rows)
+  let rows = csvString.split("\n");
+  console.log(rows);
 
   // Use the first row to get the keys for each column
-  let keys = rows[0].split(',');
+  let keys = rows[0].split(",");
 
   // Create an array to hold the final data
   let data = [];
@@ -79,11 +78,11 @@ function parseCSV(csvString) {
   // Loop through the rows
   for (let i = 1; i < rows.length; i++) {
     // Split the row on commas to get an array of columns
-    let columns = rows[i].split(',');
+    let columns = rows[i].split(",");
 
     // Create an object for this row of data
     let row = {};
-  
+
     // Use the keys array to set the values for each column
     for (let j = 0; j < keys.length; j++) {
       const key = keys[j].trim();
@@ -91,12 +90,12 @@ function parseCSV(csvString) {
 
       row[key] = value;
     }
-    
+
     // Push the row object onto the data array
     row.id = i;
     data.push(row);
   }
-  
+
   setTableData(data);
   return data;
 }
@@ -104,7 +103,7 @@ function parseCSV(csvString) {
 parseCSV(csvString);
 
 const handleUser = (id) => {
-  console.log("Clicking user..")
+  console.log("Clicking user..");
   const user = users.find((user) => user.id === id);
   const { transactions } = user;
 
@@ -114,33 +113,38 @@ const handleUser = (id) => {
   transactions.push(currentlySelectedTransaction);
 
   // Remove transaction from data
-  data = data.filter((transaction) => transaction.id !== currentlySelectedTransaction.id);
+  data = data.filter(
+    (transaction) => transaction.id !== currentlySelectedTransaction.id
+  );
 
-  console.log(users)
+  console.log(users);
 
   // Update table
   setTableData(data);
-}
+};
 
 const handleTransaction = (id) => {
-  console.log("Transaction: ", id)
+  console.log("Transaction: ", id);
   const transaction = data.find((transaction) => transaction.id === id);
 
-  console.log(data)
-  console.log(transaction)
+  console.log(data);
+  console.log(transaction);
   currentlySelectedTransaction = transaction;
-}
+};
 
-const displayUsers = (user) => {
-  const { id, name } = user;
-  const usersData = `
-    <div onclick="handleUser(${id})" data-id="${id}">
-      <p>${name}</p>
-    </div>
-  `;
+const refreshUsersDiv = () => {
+  let htmlString = "";
+  users.map((user) => {
+    const { id, name } = user;
+    const userHtml = `
+          <div onclick="handleUser(${id})" data-id="${id}">
+            <p>${name}</p>
+          </div>
+        `;
+    htmlString += userHtml;
+  });
 
-  console.log(users)
-  // Append users at the end of div
-  document.getElementById('users').innerHTML += usersData;
-}
+  document.getElementById("users").innerHTML = htmlString;
+};
 
+refreshUsersDiv();
