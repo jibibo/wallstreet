ef7a0968d8882f1c38ebefa7125252181fb5699a
Badr <badrkmc@gmail.com>
2023-01-04 22:25:35 +0500

    add ids for transactions, trim description

--- wallstreet-client/src/utils/parseCSV.js
+++ wallstreet-client/src/utils/parseCSV.js
@@ -1,31 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default function parseCSV(csvString) {
   let rows = csvString.split("\n");
-  
-  // Use the first row to get the keys for each column
-  let keys = rows[0].split(",");
-  
+
   let data = [];
   // Loop through the rows
   for (let i = 1; i < rows.length; i++) {
     // Split the row on commas to get an array of columns
     let columns = rows[i].split(",");
-    
+
     // Create an object for this row of data
     let amount = parseFloat(columns[1]);
     if (columns[0] == "withdraw")
       amount = -amount;
-    
+
     const row = {
-      id: i,
+      id: uuidv4(),
       amount,
-      description: columns[2],
+      description: columns[2].trim(),
     };
-    
+
     // Push the row object onto the data array
-    row.id = i;
     data.push(row);
   }
-  
+
   console.log(data)
   return data;
 }
