4329d602b2a51422817bd814bbb00ca9cbd84129
Badr <badrkmc@gmail.com>
2023-01-04 21:04:36 +0500

    remove misc

--- wallstreet-client/src/components/app.js
+++ wallstreet-client/src/components/app.js
@@ -2,21 +2,14 @@ import { Router } from 'preact-router';
 
 import { TransactionsContextProvider } from '../context/TransactionsContext';
 
-import Header from './header';
-
-// Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 
 const App = () => (
 	<TransactionsContextProvider>
 		<div id="app">
-			<Header />
 			<main>
 				<Router>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</main>
 		</div>
