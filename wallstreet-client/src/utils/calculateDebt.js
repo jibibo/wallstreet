d34ffe8f9c18d3ca3ef5ee0686a2688d18a869e2
Badr <badrkmc@gmail.com>
2023-01-07 13:40:46 +0500

    round total to 2 decimal places

--- wallstreet-client/src/utils/calculateDebt.js
+++ wallstreet-client/src/utils/calculateDebt.js
@@ -1,5 +1,6 @@
 export default function calculateDebt(userTransactions) {
   let total = userTransactions.reduce((acc, transaction) => (acc + transaction.amount), 0);
 
+  total = Math.round((total + Number.EPSILON) * 100) / 100;
   return total;
 }
