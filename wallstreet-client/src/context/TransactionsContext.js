dfeee65a2c27474580ad803e80aff915ff3a0f96
Badr <badrkmc@gmail.com>
2023-01-07 15:26:33 +0500

    reset saved users selected state on restart

--- wallstreet-client/src/context/TransactionsContext.js
+++ wallstreet-client/src/context/TransactionsContext.js
@@ -18,6 +18,7 @@ export const TransactionsContextProvider = ({ children }) => {
     }
 
     if (savedUsers) {
+      savedUsers.forEach(user => { user.selected = false });
       setUsers(savedUsers);
     }
   }, []);
