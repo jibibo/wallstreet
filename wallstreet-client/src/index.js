6b2109ef3aee1a745af6484a1c78ced137952e2c
Badr <badrkmc@gmail.com>
2023-01-08 21:26:57 +0500

    Revert "add loading suspense"
    This reverts commit 1f4cd933a1047919ffc07d4537bf7c4a0f4a0252.

--- wallstreet-client/src/index.js
+++ wallstreet-client/src/index.js
@@ -1,16 +1,4 @@
 import './style';
-import Loading from './components/loading';
+import App from './components/app';
 
-import { Suspense, lazy } from 'preact/compat'
-
-const App = lazy(() => import('./components/app'));
-
-const Root = () => {
-  return (
-    <Suspense fallback={<Loading />}>
-      {<App />}
-    </Suspense>
-  );
-};
-
-export default Root;
+export default App;
